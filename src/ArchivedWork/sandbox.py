5c5702786efb1c2060e7ccd83914654c93f427bd
dlinus231 <71240716+dlinus231@users.noreply.github.com>
2023-12-04 23:51:02 +0100

    Delete src/ArchivedWork directory

--- src/ArchivedWork/sandbox.py
+++
@@ -1,9 +0,0 @@
-import os
-
-print(os.getcwd())
-directories = os.listdir("archive/tom_and_jerry/tom_and_jerry/")
-directories.sort()
-
-# verifying that all images were edge-filtered
-for folderName in directories: 
-    print(folderName, len(os.listdir("archive/tom_and_jerry/tom_and_jerry/" + folderName)))
